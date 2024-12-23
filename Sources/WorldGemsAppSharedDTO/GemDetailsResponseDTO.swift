05401064f726c3980778f3b41ece0341fa3b0cf0
Victor Rodriguez Reche <victorrodri04@gmail.com>
2024-12-23 06:19:50 +0100

    Added DTOs

---
+++ Sources/WorldGemsAppSharedDTO/GemDetailsResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  GemsListRequestDTO.swift
+//  WorldGemsAppSharedDTO
+//
+//  Created by Victor Rodriguez Reche on 19/12/24.
+//
+
+import Foundation
+
+public struct GemDetailsRequestDTO: Encodable {
+  
+}
